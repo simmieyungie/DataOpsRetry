e53b0b32318d8b2913cdbd636d63a4e80ae87d5a
Simmieyungie <56706551+simmieyungie@users.noreply.github.com>
2021-07-24 21:20:46 +0100

    Updated

--- Analysis.R
+++ Analysis.R
@@ -43,7 +43,7 @@ a <- na.omit(files_n) %>%
 df <- do.call("rbind",lapply(a, read.csv))
 
 #temp data
-df <- read.csv("C:\\Users\\User\\Documents\\GitHub\\DataOpsRetry\\data\\bbnreunion 2021-06-29 23-39-26.csv")
+#df <- read.csv("C:\\Users\\User\\Documents\\GitHub\\DataOpsRetry\\data\\bbnreunion 2021-06-29 23-39-26.csv")
 
 
 #Get the distinct tweets
